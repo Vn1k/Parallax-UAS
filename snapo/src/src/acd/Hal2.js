ff732fdf830eb525f3c3af6c464b6f5817e42cbb
acd17 <127939247+acd17@users.noreply.github.com>
2023-05-14 19:07:09 +0700

    tailwind

---
+++ snapo/src/src/acd/Hal2.js
@@ -0,0 +1,10 @@
+const Hal2 = [
+    {
+        image: "",
+        title: "Living Space",
+        isi: "Our first skin is human skin. Clothes make up our second skin. If so, then isn’t our third skin made up of our living space, the walls, doors, and windows that surround the human body?",
+        name: "Chiharu Shiota"
+    }
+];
+
+export default Hal2;
