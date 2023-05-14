be5981ba31b2df900024564026612decf3bcc4d5
acd17 <127939247+acd17@users.noreply.github.com>
2023-05-14 21:23:43 +0700

    hal2

---
+++ snapo/src/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Hal2 from "./acd/Hal2";
+
+function App()
+{
+    return (
+        <div>
+            <Hal2/>
+        </div>
+    )
+}
--- snapo/src/src/acd/Hal2.js
+++ snapo/src/src/acd/Hal2.js
@@ -1,6 +1,6 @@
 const Hal2 = [
     {
-        image: "",
+        image: "lukisan.jpg",
         title: "Living Space",
         isi: "Our first skin is human skin. Clothes make up our second skin. If so, then isn’t our third skin made up of our living space, the walls, doors, and windows that surround the human body?",
         name: "Chiharu Shiota"
