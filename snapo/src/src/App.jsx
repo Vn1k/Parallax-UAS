e8e3b77fccc03d95ea71caaaad776c08029218fd
Vn1k <56496329+Vn1k@users.noreply.github.com>
2023-05-15 14:14:32 +0700

    Home Vinik

--- snapo/src/src/App.jsx
+++ snapo/src/src/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Hal2 from "./acd/Hal2";
+import Vinik from "./Vinik/Home";                        
 
 function App()
 {
     return (
         <div>
+            <Home/>
             <Hal2/>
         </div>
-    )
+    );
 }
+
+export default App;
---
+++ snapo/src/src/Vinik/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Home from './Home-data.js';
+
+function Home() {
+    return (
+        <div id="Navbar">
+            <h1>V</h1>
+        </div>
+    );
+}
+
+export default Home;
