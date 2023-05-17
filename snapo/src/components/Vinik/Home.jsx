7d3533c7b83364769a2da31fc60e29ba7da2adc9
NAS2203 <129538027+NAS2203@users.noreply.github.com>
2023-05-17 13:32:27 +0700

    Changing files paths

--- snapo/src/components/Vinik/Home.jsx
+++ snapo/src/components/Vinik/Home.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import Home from './Home-data.js';
 
 function Home() {
     return (
         <div id="Navbar">
             <h1>V</h1>
         </div>
-    );
+    )
 }
 
 export default Home;
