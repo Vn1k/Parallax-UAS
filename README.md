e74b98c0ea27ddb31d24d5eab867b8a5d277a545
Vn1k <56496329+Vn1k@users.noreply.github.com>
2023-05-17 13:08:02 +0700

    s

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Parallax UAS
- 
+# Nama Kelompok
+
--- snapo/src/src/Vinik/Home.jsx
+++ snapo/src/src/Vinik/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Home from './Home-data.js';
+import Home from './Home.js';
 
 function Home() {
     return (
