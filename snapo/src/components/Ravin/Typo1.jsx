2104c7449796437d6bdae1f9d8789466551f73ac
ravinwoenarsooo <robertus.ravin@student.umn.ac.id>
2023-05-18 15:58:22 +0700

    update

--- snapo/src/components/Ravin/Typo1.jsx
+++ snapo/src/components/Ravin/Typo1.jsx
@@ -6,9 +6,9 @@ export default function Tipo1(item){
             <div id="hands">
                 <img src={item.gmbr}/>
             </div>
-            <h1 id="judul"/>
-            <h1 id="text"/>
-            <h1 id="tahun"/>
+            <h1 id="judul">{item.judul}</h1>
+            <h1 id="text">{item.text}</h1>
+            <h1 id="tahun">{item.tahun}</h1>
         </div>
     );
 }
