b09d64a5abddf69823a4292b27e969df6da50217
acd17 <127939247+acd17@users.noreply.github.com>
2023-05-18 14:11:58 +0700

    up

--- snapo/src/components/App.jsx
+++ snapo/src/components/App.jsx
@@ -2,11 +2,7 @@ import React from "react";
 import Navbar from "../components/Vinik/Navbar";    
 import AboutUs from "../components/NAS/AboutUs";
 import core from "./NAS/core";
-<<<<<<< HEAD
 import Tipo3 from "./Acd/Tipo3";
-=======
-// import Tipo from "./Calista/Tipo";
->>>>>>> d4e2790d32300de76141941f90e0fd9580c2dad2
 
 function App()
 {
@@ -16,11 +12,7 @@ function App()
                 <h1 className="besok">Besok siang hari</h1>
             <AboutUs/>
             {document.addEventListener("turbolinks:load", core())}
-<<<<<<< HEAD
             <Tipo3 />
-=======
-            {/* <Tipo /> */}
->>>>>>> d4e2790d32300de76141941f90e0fd9580c2dad2
         </div>
     );
 }
