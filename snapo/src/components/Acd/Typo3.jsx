a983121843579dd4c9cfd189e4cca896ade5c8ca
acd17 <127939247+acd17@users.noreply.github.com>
2023-05-19 23:17:02 +0700

    css hal2

--- snapo/src/components/Acd/Typo3.jsx
+++ snapo/src/components/Acd/Typo3.jsx
@@ -9,8 +9,11 @@ function Typo3(param)
                 <img id={param.id} src={param.image3}/>
             </div>
             <h1 id="title">{param.title}</h1>
-            <h3 id="text">{param.text}</h3>
+            <p id="text">{param.text}</p>
             <h3 id="author">{param.author}</h3>
+            <div id="rectangle"></div>
+            <div id="rectangle1"></div>
+            <div id="rectangle2"></div>
         </div>
     );
 }
--- snapo/src/components/Acd/tipou3.js
+++ snapo/src/components/Acd/tipou3.js
@@ -2,7 +2,7 @@ const tipou3 = [
     {
         id: 1,
         image: require("../../ASET/Images/lukisan.jpg"),
-        title: "Living Space",
+        title: "Living \nSpace",
         text: "Our first skin is human skin. Clothes make up our second skin. If so, then isn’t our third skin made up of our living space, the walls, doors, and windows that surround the human body?",
         author: "Chiharu Shiota"
     }
