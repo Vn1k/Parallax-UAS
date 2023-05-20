f8ac82ed0729c4ec8d3e95ee72e622768728f4e5
ravinwoenarsooo <robertus.ravin@student.umn.ac.id>
2023-05-20 15:53:36 +0700

    update dikit

--- snapo/src/components/Ravin/Typo1.jsx
+++ snapo/src/components/Ravin/Typo1.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 
-function Tipo1(item){
+function Tipo1(){
     return(
         <div id="ravin">
             <div id="hands">
-                <img alt="tangan" src="../../ASET/Images/hands.jpg"/>
+                <img src="../../ASET/Images/hands.jpg" alt="hands"/>
             </div>
+            <div id="garis"></div>
+            {/* garis judul */}
             <h1 id="judul">THREADS</h1>
-            <h1 id="text">Threads become tangled, intertwined, broken off, unraveld. They constanly reflect a part of my mental state, as if they were expressing the state of human relationships.</h1>
-            <h1 id="tahun">2019</h1>
+            <h2 id="tulisan">Threads become tangled, intertwined, broken off, unraveld. They constanly reflect a part of my mental state, as if they were expressing the state of human relationships.</h2>
+            <h2 id="tahun">2019</h2>
+            <div id="kotak1"></div>
+            {/* persegi panjang kebawah */}
+            <div id="gariskotak"></div>
+            {/* yang nempel di foto */}
+            <div id="lingkaran"></div>
+            {/* dibawah foto */}
+            <div id="gariskecil"></div> 
+            {/* dibawah tulisan */}
         </div>
     );
 }
