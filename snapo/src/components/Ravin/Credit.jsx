cc8df9a924c1a536906ece39c129837f23d518ba
ravinwoenarsooo <robertus.ravin@student.umn.ac.id>
2023-05-21 18:56:08 +0700

    update credit on work

--- snapo/src/components/Ravin/Credit.jsx
+++ snapo/src/components/Ravin/Credit.jsx
@@ -5,16 +5,16 @@ function Credit(){
     return(
        <div id="Credit">
             <h1 id="jfoot">Lorem Ipsum</h1>
-            <div id="gm">
-                <img src={gmbrf} alt="footer"/>
-            </div>
-            <p id="dfoot">Lorem ipsum dolor sit amet duadba jandw jsbdwb ndjwdw iqjsi ihdiq ajbabd ihsiq. </p>
+            <p id="dfoot">Lorem ipsum dolor sit amet duadba jandw jsbdwb ndjwdw iqjsi ihdiq ajbabd ihsiq.</p>
             <p id="Ref1">References1</p>
             <p id="Ref2">References2</p>
             <p id="Ref3">References3</p>
             <p id="Ref4">References4</p>
             <div id="vert"></div>
             <div id="hor"></div>
+            <div id="gm">
+                <img src={gmbrf} alt="ppics"/>
+            </div>
        </div> 
     );
 }
