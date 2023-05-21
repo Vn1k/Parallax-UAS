2e8ca5eb256a4b66b070481a0f5e218b2cbbf6f9
acd17 <127939247+acd17@users.noreply.github.com>
2023-05-22 01:58:42 +0700

    start work

--- snapo/src/components/App.jsx
+++ snapo/src/components/App.jsx
@@ -6,7 +6,8 @@ import Tipo3 from "./Acd/Tipo3";
 import Typo1 from "./Ravin/Typo1";
 import Home from "./Vinik/Home/Home";
 import Tipo2 from "./Acd/Tipo2";
-import Gallery from "./Ravin/Gallery"; 
+import Gallery from "./Ravin/Gallery";
+import Work from "./Acd/Work"; 
 import Credit from "./Ravin/Credit";
 
 function App()
@@ -17,8 +18,9 @@ function App()
             <Home/>
             <Tipo3 />
             <Typo1/>
-            {/* <Tipo2 /> */}
+            <Tipo2 /> 
             <Gallery/>
+            <Work/>
             <AboutUs/>
             <Credit/>
         </div>
