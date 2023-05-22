1756ffc47196472954c8ea9480cd4f7fc1bab649
WarmHatori <warmhatori@gmail.com>
2023-05-22 14:05:38 +0700

    up

--- snapo/src/components/Calista/cal.js
+++ snapo/src/components/Calista/cal.js
@@ -3,7 +3,6 @@ const cal = [
         text: "Hai, namaku Calista Belva dari jurusan Informatika 2022",
         img1: require("../src/ASET/bgshcal.JPG"),
         img2: require("../src/ASET/ppcal.jpg"),
-        // video: require("../src/ASET/Cal/vidcal.mov"),
     },
 ];
 
--- snapo/src/components/Calista/tipo.js
+++ snapo/src/components/Calista/tipo.js
@@ -3,7 +3,7 @@ const tipo = [
         text: "Going from life to death is not an extinguishment but a process of dissolving into something vaster. If this is the case, then there is no longer any need to be afraid of death. Both dying and living belong to the same dimension.",
         img1: require("../../ASET/Images/chair roda.jpg"),
         img2: require("../../ASET/Images/kain.jpg"),
-    },
+    }
 ];
 
 export default tipo;
