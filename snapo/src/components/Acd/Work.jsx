d781bb49f2e4aa9e023c8d82e16e7bb5296e65ae
NAS2203 <129538027+NAS2203@users.noreply.github.com>
2023-05-22 12:08:22 +0700

    minor update acd

--- snapo/src/components/Acd/Work.jsx
+++ snapo/src/components/Acd/Work.jsx
@@ -3,16 +3,16 @@ import React from "react";
 function Work ()
 {
     const Imgine1 = require("../../ASET/Images/piano crop.jpg");
-   /*const Logou = require("../../ASET/SVG/SNAPO-WHITE.jpg");*/
+    const Logou = require("../../ASET/SVG/LOGO_SNAPO.svg");
 
     return (
         <div id="containerWork">
             <div id="imgWork">
                 <img src={Imgine1} alt="piano" />
             </div>
-            {/*<div id="logoWork">
+            <div id="logoWork">
                 <img src={Logou} alt="logo" />
-             </div>*/}
+             </div>
             <div id="textWork">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</div>
             <div id="creditWork">Created by |</div>
             <div id="nameWork">M O Y S H I</div>
