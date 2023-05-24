b2c241e5499b5ce57c9a21c2408d6e45143d2de0
ravinwoenarsooo <robertus.ravin@student.umn.ac.id>
2023-05-24 13:04:42 +0700

    patch

--- snapo/src/components/App.jsx
+++ snapo/src/components/App.jsx
@@ -5,7 +5,7 @@ import Tipo3 from "./Acd/Tipo3";
 import Typo1 from "./Ravin/Typo1";
 import Home from "./Vinik/Home/Home";
 import Tipo2 from "./Acd/Tipo2";
-import Gallery from "./Ravin/Gallery";
+import Credit from "./Ravin/Credit";
 import Work from "./Acd/Work"; 
 import Footer from "./Ravin/Footer";
 
@@ -21,7 +21,7 @@ function App()
             {/* <Gallery/>  */}
             {/* <Work/> */}
             <AboutUs/> {/* Corrected Unfinished*/}
-            {/* <Gallery/> */}
+            {/* <Credit/> */}
             <Work/>
             {/* <AboutUs/> Corrected Unfinished */}
             {/* <Footer/> */}
