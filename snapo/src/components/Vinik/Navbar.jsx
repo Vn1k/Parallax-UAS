e234e897413b7ad576c74d016bbcacae80869775
Vinik <56496329+Vn1k@users.noreply.github.com>
2023-05-24 20:03:12 +0700

    Navbar Tailwind

--- snapo/src/components/Vinik/Navbar.jsx
+++ snapo/src/components/Vinik/Navbar.jsx
@@ -2,35 +2,36 @@ import React from "react";
 import './nabar.css';
 import { ReactComponent as Lognav } from '../../ASET/SVG/SNAPO.svg';
 
-function Navbar () {
+function Navbar() {
     return (
-        <div id="navbar">
-            <nav>
-                <div id="nav-title">
-                    <Lognav/>
+      <nav className="fixed py-4 px-8" id="navbar">
+        <div className="flex items-center justify-between">
+            
+            <ul className="flex space-x-4">
+                <li>
+                <a href="#" className="text-white">About Us</a>
+                </li>
+                <li>
+                <a href="#" className="text-white">Home</a>
+                </li>
+                <li>
+                <a href="#" className="text-white">Gallery</a>
+                </li>
+                <li>
+                <a href="#" className="text-white">Snapo</a>
+                </li>
+                <li>
+                <a href="#" className="text-white">Credit</a>
+                </li>
+            </ul>
+            <div className="flex items-start">
+                <div className="mr-4">
+                    <Lognav className="h-8 w-8" id='logo' />
                 </div>
-                <div id="nav-link">
-                    <ul>
-                        <li>
-                            <a>About Us</a>
-                        </li>
-                        <li>
-                            <a>Home</a>
-                        </li>
-                        <li>
-                            <a>Gallery</a>
-                        </li>
-                        <li>
-                            <a>Snapo</a>
-                        </li>
-                        <li>
-                            <a>Credit</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
+            </div>
         </div>
+      </nav>
     );
-}
-
-export default Navbar;
+  }
+  
+  export default Navbar;
