fed0ffa19ee86f8c3293205cea60bd60e3c7713f
ravinwoenarsooo <robertus.ravin@student.umn.ac.id>
2023-05-24 13:07:18 +0700

    Individual development

---
+++ snapo/src/components/Ravin/Individual.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Ravin(){
+    return(
+        <div id="container">
+            <div>
+                
+            </div>
+        </div>
+    );
+}
+
+export default Ravin;
