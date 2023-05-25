329f75a72f7d5d83dbe6bb3554b47c3af8746836
Vn1k <56496329+Vn1k@users.noreply.github.com>
2023-05-25 10:53:30 +0700

    Revert "Animation for navbar"
    This reverts commit 44e46e09c3e6919fc501d6622d22072a8587e6c3.

--- snapo/src/components/Vinik/Navbar.jsx
+++ snapo/src/components/Vinik/Navbar.jsx
@@ -3,29 +3,48 @@ import "./nabar.css";
 import { ReactComponent as Lognav } from "../../ASET/SVG/SNAPO.svg";
 import {
   handleScroll,
-  scrollToTop
+  toggleDropdown,
+  closeDropdown,
+  handleLogoHover,
+  handleLogoLeave,
 } from "./navbarFunction";
 
 function Navbar() {
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [isLogoHovered, setIsLogoHovered] = useState(false);
   const [isNavbarVisible, setIsNavbarVisible] = useState(true);
   const [prevScrollPos, setPrevScrollPos] = useState(window.pageYOffset);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
   useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
 
     window.addEventListener("scroll", handleScroll(setIsNavbarVisible, setPrevScrollPos, prevScrollPos));
+    window.addEventListener("resize", handleResize);
 
     return () => {
       window.removeEventListener("scroll", handleScroll(setIsNavbarVisible, setPrevScrollPos, prevScrollPos));
+      window.removeEventListener("resize", handleResize);
     };
   }, [prevScrollPos]);
 
   return (
-    <nav className={`fixed py-4 px-8 ${isNavbarVisible ? "visible" : ""}`} id="navbar">
+    <nav
+      className={`fixed py-4 px-8 ${isNavbarVisible ? "visible" : "hidden"}`}
+      id="navbar"
+    >
       <div className="flex items-center justify-between">
-        <ul className={`flex space-x-4`}>
+        <ul className={`flex space-x-4 ${showDropdown ? "active" : ""}`}>
           <li>
             <a href="#AboutUs" className="text-white">
               About Us
+            </a>
+          </li>
+          <li>
+            <a href="#" className="text-white">
+              Home
             </a>
           </li>
           <li>
@@ -44,10 +63,43 @@ function Navbar() {
             </a>
           </li>
         </ul>
-        <div className={`flex items-start mr-4`}>
-          <a href="#top" onClick={scrollToTop}>
-            <Lognav className="h-8 w-8" id="logo" />
-          </a>
+        <div
+          className={`flex items-start mr-4 ${
+            isLogoHovered ? "cursor-pointer" : ""
+          }`}
+          onMouseEnter={handleLogoHover(windowWidth, setIsLogoHovered)}
+          onMouseLeave={handleLogoLeave(setIsLogoHovered)}
+        >
+          <Lognav className="h-8 w-8" id="logo" />
+          {showDropdown && (
+            <ul className="absolute mt-8 bg-white p-2 shadow">
+              <li>
+                <a href="#AboutUs" className="text-gray-800">
+                  About Us
+                </a>
+              </li>
+              <li>
+                <a href="#" className="text-gray-800">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="#" className="text-gray-800">
+                  Gallery
+                </a>
+              </li>
+              <li>
+                <a href="#" className="text-gray-800">
+                  Snapo
+                </a>
+              </li>
+              <li>
+                <a href="#" className="text-gray-800">
+                  Credit
+                </a>
+              </li>
+            </ul>
+          )}
         </div>
       </div>
     </nav>
