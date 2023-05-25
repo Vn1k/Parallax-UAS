70ceead039cec3d1853b0aab6018af8ec6831d73
ravinwoenarsooo <robertus.ravin@student.umn.ac.id>
2023-05-25 11:11:32 +0700

    Major Update Gallery2

--- snapo/src/components/Ravin/Gallery2.jsx
+++ snapo/src/components/Ravin/Gallery2.jsx
@@ -1,9 +1,27 @@
 import React from "react";
 
 function Gallery2(){
+    const gambar1 = require("../../ASET/Images/benang gantung.jpg");
+    const gambar2 = require("../../ASET/Images/miniatur.jpg");
+    const gambar3 = require("../../ASET/Images/piano.jpg");
+    const gambar4 = require("../../ASET/Images/kain.jpg");
+    const gambar5 = require("../../ASET/Images/salib.jpg");
+    const gambar6 = require("../../ASET/Images/jubah.jpg")
+
     return(
-        <div>
-            {/* ceritanya ini isi */}
+        <div id="con">
+            <div id="gro">
+                <img id="Art1" src={gambar1} alt="Art" />
+                <img id="Art2" src={gambar2} alt="Art" /> 
+            </div>
+            <div id="gro">
+                <img id="Art3" src={gambar3} alt="Art" />
+                <img id="Art4" src={gambar4} alt="Art" /> 
+            </div>
+            <div id="gro">
+                <img id="Art5" src={gambar5} alt="Art" />
+                <img id="Art6" src={gambar6} alt="Art" /> 
+            </div>
         </div>
     );
 }
