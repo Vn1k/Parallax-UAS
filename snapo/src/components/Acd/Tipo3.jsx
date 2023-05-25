9f0789276dd769cbfb5685e75ad313f3cd388afa
acd17 <127939247+acd17@users.noreply.github.com>
2023-05-25 09:54:14 +0700

    TIPO 3 BENAR!

--- snapo/src/components/Acd/Tipo3.jsx
+++ snapo/src/components/Acd/Tipo3.jsx
@@ -1,23 +1,24 @@
 import React from "react";
 import './tipo3.css';
-
 function Tipo3()
 {
     const image1 = require("../../ASET/Images/lukisan.jpg")
     return (
         <div id="container3">
             <div id="image3">
-                <img src={image1}/>
+                <img src={image1} alt=""/>
+                <h2 id="kutip">"</h2>
+                <h1 id="title">Living <br /> Space</h1>
+                <p id="text">Our first skin is human skin. Clothes make up our second skin. If so, then isn’t our third skin made up of our living space, the walls, doors, and windows that surround the human body?</p>
+                <h5 id="author">Chiharu Shiota</h5>
+                <div id="rectangle"></div>
+                <div id="rectangle1"></div>
+                <div id="line"></div>
+                <div id="dot"></div>
             </div>
-            <h2 id="kutip">"</h2>
-            <h1 id="title">Living <br /> Space</h1>
-            <p id="text">Our first skin is human skin. Clothes make up our second skin. If so, then isn’t our third skin made up of our living space, the walls, doors, and windows that surround the human body?</p>
-            <h5 id="author">Chiharu Shiota</h5>
-            <div id="rectangle"></div>
-            <div id="rectangle1"></div>
+            
             <div id="rectangle2"></div>
-            <div id="line"></div>
-            <div id="dot"></div>
+            
         </div>
     );
 }
