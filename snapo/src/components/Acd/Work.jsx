4dd23c688b94f6c984aa1e2fbf34ef7321b637bd
WarmHatori <warmhatori@gmail.com>
2023-05-25 15:50:17 +0700

    up

--- snapo/src/components/Acd/Work.jsx
+++ snapo/src/components/Acd/Work.jsx
@@ -16,14 +16,7 @@ function Work ()
             <div id="textWork">SNAPO merupakan sebuah singkatan dari SNAP yaitu foto dan PO adalah typography. Sehingga, SNAPO menampilkan sebuah website yang berisikan gallery foto 'Museum' dan diberikan sebuah typography untuk menambahkan estetika sebuah foto.</div>
             <div id="creditWork">Created by | <strong>M O Y S H I</strong></div>
             <div id="recWork1"></div>
-            <div id="recWork2"></div>
-            <div id="color3">
-                <div id="recWork3"></div>
-                <div id="recWork4"></div>
-                <div id="recWork5"></div>
-                <div id="recWork6"></div>
-            </div>
-            <div id="carouselWork">
+            <div id="recWork2">
                 <div id="imgWork1">
                     <img src={Imgine1} alt="piano" />
                 </div>
@@ -34,6 +27,23 @@ function Work ()
                     <img src={Imgine3} alt="lukisan4" />
                 </div>
             </div>
+            <div id="color3">
+                <div id="recWork3"></div>
+                <div id="recWork4"></div>
+                <div id="recWork5"></div>
+                <div id="recWork6"></div>
+            </div>
+            {/* <div id="carouselWork">
+                <div id="imgWork1">
+                    <img src={Imgine1} alt="piano" />
+                </div>
+                <div id="imgWork2">
+                    <img src={Imgine2} alt="bikai2" />
+                </div>
+                <div id="imgWork3">
+                    <img src={Imgine3} alt="lukisan4" />
+                </div>
+            </div> */}
         </div>
     );
 }
