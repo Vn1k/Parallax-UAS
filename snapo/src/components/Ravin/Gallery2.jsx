4e05f8c5ce3f563a6cf2c0b48657d8edfcc0002b
WarmHatori <warmhatori@gmail.com>
2023-05-25 14:28:28 +0700

    up

--- snapo/src/components/Ravin/Gallery2.jsx
+++ snapo/src/components/Ravin/Gallery2.jsx
@@ -4,7 +4,7 @@ function Gallery2(){
     const gambar1 = require("../../ASET/Images/benang gantung.jpg");
     const gambar2 = require("../../ASET/Images/miniatur.jpg");
     const gambar3 = require("../../ASET/Images/piano.jpg");
-    const gambar4 = require("../../ASET/Images/kain.jpg");
+    const gambar4 = require("../../ASET/Images/kaincal.JPG");
     const gambar5 = require("../../ASET/Images/salib.jpg");
     const gambar6 = require("../../ASET/Images/jubah.jpg")
 
@@ -16,7 +16,7 @@ function Gallery2(){
             </div>
             <div id="gro">
                 <img id="Art3" src={gambar3} alt="Art" />
-                <img id="Art4" src={gambar4} alt="Art" /> 
+                {/* <img id="Art4" src={gambar4} alt="Art" />  */}
             </div>
             <div id="gro">
                 <img id="Art5" src={gambar5} alt="Art" />
