4e49cd7a99029a9b05c23b43f647a8e5e6d06440
NAS2203 <129538027+NAS2203@users.noreply.github.com>
2023-05-25 14:32:23 +0700

    update

--- snapo/src/index.js
+++ snapo/src/index.js
@@ -2,18 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
 
-window.addEventListener('mousemove', function(event) {
-    const screenWidth = window.innerWidth;
-    const screenHeight = window.innerHeight;
-    const mousePositionX = event.clientX;
-    const mousePositionY = event.clientY;
-    const panAmountX = (mousePositionX / screenWidth) * 0.5; // Adjust the pan speed as needed
-    const panAmountY = (mousePositionY / screenHeight) * 0.1; // Adjust the pan speed as needed
-  
-    document.body.style.transform = `translate(-${panAmountX}%, -${panAmountY}%)`;
-  });
-  
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
