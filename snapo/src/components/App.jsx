374b1ad778511f84b25e710acf41ab9faeb4c7f2
ravinwoenarsooo <robertus.ravin@student.umn.ac.id>
2023-05-25 10:17:20 +0700

    update lg

--- snapo/src/components/App.jsx
+++ snapo/src/components/App.jsx
@@ -10,7 +10,7 @@ import Gallery from "./Ravin/Gallery";
 import Work from "./Acd/Work";
 import CardCredit from "./Calista/CardCredit";
 import Footer from "./Ravin/Footer";
-import Gallery1 from "./Ravin/Gallery1";
+import Gallery1 from "./Ravin/Gallery1"; //ini yang di link 
 
 function App()
 {
@@ -29,7 +29,6 @@ function App()
             <Tipo2 /> {/* Corrected Finished */}
             {/* <Credita /> */}
             <Gallery/>
-            <Gallery1/>
             <Work/>
             <AboutUs/> {/* Corrected Unfinished*/}
             {/* <Footer/> */}
