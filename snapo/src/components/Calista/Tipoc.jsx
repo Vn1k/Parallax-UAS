6c7012d84c55f9902d07df018dc1bac8b1d43f88
Vn1k <56496329+Vn1k@users.noreply.github.com>
2023-05-25 09:47:27 +0700

    images cal

--- snapo/src/components/Calista/Tipoc.jsx
+++ snapo/src/components/Calista/Tipoc.jsx
@@ -6,8 +6,10 @@ function Tipoc(props){
         <div id="cardTipoc">
                 <div id="headingTipoc">{props.title}</div>
                 <div id="textTipoc">{props.text}</div>
-                <div id="image1"><img id="img1" src={props.img1} /></div>
-                <div id="image2"><img id="img2" src={props.img2} /></div>
+                <div id="images">
+                    <img id="img1" src={props.img1} />
+                    <img id="img2" src={props.img2} />
+                </div>
                 <div id="line"><div class="line"></div></div>
                 <div id="line123">
                     <div class="line1"></div>
