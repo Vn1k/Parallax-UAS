f8c48ed53de68da4bc8155142f3ba67ca68eebc7
NAS2203 <129538027+NAS2203@users.noreply.github.com>
2023-05-26 00:24:10 +0700

    Testing Animation

--- snapo/src/components/Acd/Tipo3.jsx
+++ snapo/src/components/Acd/Tipo3.jsx
@@ -12,9 +12,9 @@ function Tipo3()
                 <img src={image1} alt=""/>
                 <h2 id="kutip">"</h2>
                 <h1 id="title">Living <br /> Space</h1>
-                <p id="text">Our first skin is human skin. Clothes make up our second skin. If so, then isn’t our third skin made up of our living space, the walls, doors, and windows that surround the human body?</p>
+                <p data-aos="fade-up" data-aos-offset="-800" data-aos-anchor-placement="top-center" id="text">Our first skin is human skin. Clothes make up our second skin. If so, then isn’t our third skin made up of our living space, the walls, doors, and windows that surround the human body?</p>
                 <h5 id="author">Chiharu Shiota</h5>
-                <div data-aos="fade-up" data-aos-anchor-placement="center-center" id="rectangle"></div>
+                <div id="rectangle"></div>
                 <div id="rectangle1"></div>
                 <div id="line"></div>
                 <div id="dot"></div>
