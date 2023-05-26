1fd55060916fc42e42d4f3825c59602efe3c268b
ravinwoenarsooo <robertus.ravin@student.umn.ac.id>
2023-05-26 16:31:57 +0700

    Footer Rework

--- snapo/src/components/Ravin/Footer.jsx
+++ snapo/src/components/Ravin/Footer.jsx
@@ -3,17 +3,27 @@ import React from "react";
 function Footer(){
     const gmbrf = require("../../ASET/Images/footer.jpg");
     return(
-       <div id="Credit">
-            <h1 id="jfoot">Lorem Ipsum</h1>
-            <p id="dfoot">Lorem ipsum dolor sit amet duadba jandw jsbdwb ndjwdw iqjsi ihdiq ajbabd ihsiq.</p>
-            <p id="Ref1">References1</p>
-            <p id="Ref2">References2</p>
-            <p id="Ref3">References3</p>
-            <p id="Ref4">References4</p>
-            <div id="vert"></div>
-            <div id="hor"></div>
-            <div id="gm">
-                <img src={gmbrf} alt="ppics"/>
+       <div id="Footer">
+            <div>
+                <img id="gm" src={gmbrf} alt="gambarfooter" />
+            </div>
+            <div id="desc">
+                <h1 id="jfoot">Lorem Ipsum</h1>
+                <p id="dfoot">Lorem ipsum dolor sit amet duadba jandw jsbdwb ndjwdw iqjsi ihdiq ajbabd ihsiq.</p>
+            </div>
+            <div id="footerw">
+                <div>
+                    <p id="Ref1">References1</p>
+                </div>
+                <div>
+                    <p id="Ref2">References2</p>
+                </div>
+                <div>
+                    <p id="Ref3">References3</p>
+                </div>
+                <div>
+                    <p id="Ref4">References4</p>  
+                </div>            
             </div>
        </div> 
     );
