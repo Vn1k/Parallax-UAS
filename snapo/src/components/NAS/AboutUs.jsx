54144614af2efd4b7d0d24a11d11a4751d79cb3a
NAS2203 <129538027+NAS2203@users.noreply.github.com>
2023-05-26 23:24:53 +0700

    Paralax About Us

--- snapo/src/components/NAS/AboutUs.jsx
+++ snapo/src/components/NAS/AboutUs.jsx
@@ -7,6 +7,7 @@ import ACDProfile from "../Acd/ACDprofile";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import Rprofile from "../Ravin/Rprofile";
+import anime from "animejs";
 
 export const NameContext = React.createContext();
 
@@ -28,10 +29,28 @@ function AboutUs() {
     }, 100);
   }
 
+  const [cursorPosition, setCursorPosition] = useState({x:0, y:0});
+  useEffect(() => {
+    anime({
+      targets: "#bg-image-about-us img",
+      translateX: -cursorPosition.x,
+      translateY: -cursorPosition.y,
+      duration: 1000,
+      easing: 'easeOutQuad',
+    });
+  }, [cursorPosition]);
+  
+
   return (
     <NameContext.Provider value={{ name, setName, aboutIndividualRef }}>
-      <div data-aos="fade-left" data-aos-easing="ease-out-sine" data-aos-anchor-placement="top-center" id="about-us">
-        <img id="bg-image-about-us" src={GMBRUS}/>
+      <div onMouseMove={(event)=>{
+          let movement_x = event.clientX * 0.05;
+          let movement_y = event.clientY * 0.05;
+          setCursorPosition({x: movement_x, y: movement_y})
+        }} data-aos="fade-left" data-aos-easing="ease-out-sine" data-aos-anchor-placement="top-center" id="about-us">
+        <div id="bg-image-about-us">
+          <img class="selectDisable" draggable="false" style={{position: "absolute", top: -cursorPosition.y, left: -cursorPosition.x}} src={GMBRUS}/>
+        </div>
         <Individuals onImageClick={handleImageClick} />
       </div>
       <div id="about-individual" className="Invisible" ref={aboutIndividualRef}>
