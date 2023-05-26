95ef2dd6f4069085c78937f00477aec3ab5d92df
acd17 <127939247+acd17@users.noreply.github.com>
2023-05-26 11:47:32 +0700

    acd

--- snapo/src/components/Acd/ACDprofile.jsx
+++ snapo/src/components/Acd/ACDprofile.jsx
@@ -1,40 +1,64 @@
-import React, { useContext } from "react";
-import { NameContext } from "../NAS/AboutUs";
-import BackButton from "../NAS/BackButton";
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-function ACDprofile() {
-  const { name, setName, aboutIndividualRef } = useContext(NameContext);
+import './ACDprof.css';
 
-  function handleResetClick() {
-    setTimeout(() => {
-      setName(null);
-      aboutIndividualRef.current.classList.add("Invisible");
-    },1500);
-    aboutIndividualRef.current.style.left = "100%";
-    document.body.style.overflow = "scroll";
+import { EffectCoverflow, Pagination, Navigation } from 'swiper';
 
-  }
+import slide_image_1 from '../../ASET/ACD/IMG-20230414-WA0019.jpg';
+import slide_image_2 from '../../ASET/ACD/IMG_20230524_234558.jpg';
+import slide_image_3 from '../../ASET/ACD/IMG_20230429_112720.jpg';
 
-  const Me1 = require("../../ASET/ACD/IMG_20230429_112415.jpg");
-  const Me2 = require("../../ASET/ACD/IMG_20230524_234558.jpg");
-  const Me3 = require("../../ASET/ACD/IMG_20230429_112720.jpg");
-  const Me4 = require("../../ASET/ACD/IMG_20230524_234615.jpg");
-
+function App() {
   return (
-    <>
-      <BackButton onClick={handleResetClick} />
-        <div id="containerACD">
-          <div id="namaACD">Amanda Citra Dewanti</div>
-          <div id="nimACD">00000066344</div>
-          <div id="iamgeACD">
-            <img src={Me1} alt="" id="me1" />
-            <img src={Me2} alt="" id="me2" />
-            <img src={Me3} alt="" id="me3" />
-            <img src={Me4} alt="" id="me4" />
-           </div>
+    <div className="container">
+      <h1 className="name">Amanda Citra Dewanti</h1>
+      <h3 className="nim">00000066344</h3>
+      <Swiper
+        effect={'coverflow'}
+        grabCursor={true}
+        centeredSlides={true}
+        loop={true}
+        slidesPerView={'auto'}
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 0,
+          depth: 100,
+          modifier: 2.5,
+        }}
+        pagination={{ el: '.swiper-pagination', clickable: true }}
+        navigation={{
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+          clickable: true,
+        }}
+        modules={[EffectCoverflow, Pagination, Navigation]}
+        className="swiper_container"
+      >
+        <div id="imgslide">
+          <SwiperSlide>
+            <img src={slide_image_1} alt="slide_image" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={slide_image_2} alt="slide_image" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={slide_image_3} className='img3' alt="slide_image" />
+          </SwiperSlide>
         </div>
-    </>
+        
+        <div className="slider-controler">
+          <div className="swiper-button-prev slider-arrow">
+            <ion-icon name="arrow-back-outline"></ion-icon>
+          </div>
+          <div className="swiper-button-next slider-arrow">
+            <ion-icon name="arrow-forward-outline"></ion-icon>
+          </div>
+          <div className="swiper-pagination"></div>
+        </div>
+      </Swiper>
+    </div>
   );
 }
 
-export default ACDprofile;
+export default App;
