b19288eba5fa1dc2d239ec8ba1a264a50e788d33
acd17 <127939247+acd17@users.noreply.github.com>
2023-05-26 15:49:02 +0700

    Update App.jsx

--- snapo/src/components/App.jsx
+++ snapo/src/components/App.jsx
@@ -1,12 +1,5 @@
-<<<<<<< Updated upstream
 import React, { useEffect, useState } from "react";
 import Navbar from "../components/Vinik/Navbar";
-=======
-//ini komponen untuk nyusun pages main menu
-//Pages
-import React from "react";
-import Navbar from "../components/Vinik/Navbar";    
->>>>>>> Stashed changes
 import AboutUs from "../components/NAS/AboutUs";
 
 import Tipo3 from "./Acd/Tipo3";
@@ -18,15 +11,9 @@ import Gallery from "./Ravin/Gallery";
 import Work from "./Acd/Work";
 import CardCredit from "./Calista/CardCredit";
 import Footer from "./Ravin/Footer";
-<<<<<<< Updated upstream
-import Gallery1 from "./Ravin/Gallery1"; //ini yang di link
-import Gallery2 from "./Ravin/Gallery2"; //ini yang di link
-=======
->>>>>>> Stashed changes
 import { ScrollProvider } from "./ScrollContext";
 import LoadingScreen from './LoadingScreen';
 
-<<<<<<< Updated upstream
 function App() {
   return (
     <>
@@ -49,28 +36,3 @@ function App() {
 }
 
 export default App;
-=======
-function HomeMenu()
-{
-    return (
-        <ScrollProvider>
-            <Navbar/>{/* <Navbar/> */}
-            <Home/> {/* Corrected Finished */}
-            <Tipo3 /> {/* Corrected Finished */}
-            <Typo1/> {/* Corrected Finished */}
-            <CardTipoc /> 
-            {/* <Tipo2/> */}
-            {/* <Work/> */}
-            <CardCredit />
-            {/* <Tipoc />  */}
-            {/* <Credita /> */}
-            <Gallery/>
-            <Work/>
-            <AboutUs/> {/* Corrected Unfinished*/}
-            <Footer/>
-        </ScrollProvider>
-    )
-}
-
-export default HomeMenu;
->>>>>>> Stashed changes
