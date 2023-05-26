bd75e1db799a46e7fd8e902f9945123aaf60c478
ravinwoenarsooo <robertus.ravin@student.umn.ac.id>
2023-05-26 08:16:43 +0700

    function

--- snapo/src/components/Ravin/Gallery.jsx
+++ snapo/src/components/Ravin/Gallery.jsx
@@ -4,17 +4,28 @@ function Gallery(){
     const IMGURL0 = require("../../ASET/Images/borobudur.jpg");
     const IMGURL1 = require("../../ASET/Images/patung.jpg");
     const IMGURL2 = require("../../ASET/Images/lukisan3.jpg");
+    
+    function handleClickOne(){
+         alert("TES1");
+    }
+    function handleClickTwo(){
+        alert("TES2");
+    }
+    function handleClickThree(){
+        alert("TES3");
+    }
+
     return(
         <div id="gallery">
             <div id="group">
                 <div>
-                    <img id="one" src={IMGURL0} alt="borobudur"/>
+                    <img id="one" onClick={handleClickOne} src={IMGURL0} alt="borobudur"/>
                 </div>
                 <div>
-                    <img id="two" src={IMGURL1} alt="lukisan3"/>
+                    <img id="two" onClick={handleClickTwo} src={IMGURL1} alt="lukisan3"/>
                 </div>
                 <div>
-                    <img id="three" src={IMGURL2} alt="patung"/>
+                    <img id="three" onClick={handleClickThree} src={IMGURL2} alt="patung"/>
                 </div>
             </div>
         </div>
