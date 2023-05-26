706b769532134993473dc842259c71742f051e43
NAS2203 <129538027+NAS2203@users.noreply.github.com>
2023-05-26 11:56:47 +0700

    Tipo1 animation full

--- snapo/src/components/Acd/Tipo3.jsx
+++ snapo/src/components/Acd/Tipo3.jsx
@@ -9,7 +9,7 @@ function Tipo3()
     return (
         <div id="container3">
             <div id="image3">
-                <img data-aos="fade-up" data-aos-offset="-600" data-aos-anchor-placement="top-center" src={image1} alt=""/>
+                <img data-aos="fade-up" data-aos-easing="ease-out-sine" data-aos-offset="-600" data-aos-anchor-placement="top-center" src={image1} alt=""/>
                 <h2 data-aos="fade-left" data-aos-offset="-1050" data-aos-anchor-placement="center-center" id="kutip">"</h2>
                 <h1 data-aos="fade-left" data-aos-offset="-800" data-aos-anchor-placement="center-center" id="title">Living <br /> Space</h1>
                 <p data-aos="fade-left" data-aos-offset="-700" data-aos-anchor-placement="top-center" id="text">Our first skin is human skin. Clothes make up our second skin. If so, then isn’t our third skin made up of our living space, the walls, doors, and windows that surround the human body?</p>
--- snapo/src/components/Ravin/Typo1.jsx
+++ snapo/src/components/Ravin/Typo1.jsx
@@ -5,20 +5,20 @@ function Tipo1(){
     // reaksi jujur gw pas tau harus dikasih require: oalahh 😅😅 zizizizi hebat vin
     return(
         <div id="ravin">
-            <div id="garisatas"></div>
-            <h1 id="judul">THREADS</h1>
-            <h1 id="awaw">"</h1>
-            <h2 id="tulisan">Threads become tangled, intertwined, broken off, unraveld. They constanly reflect a part of my mental state, as if they were expressing the state of human relationships.</h2>
-            <h2 id="tahun">2019</h2>
-            <div id="kotak1"></div>
+            <div data-aos="fade-right" data-aos-easing="ease-out-sine" data-aos-offset="-390" data-aos-anchor-placement="top-center" id="garisatas"></div>
+            <h1 data-aos="fade-right" data-aos-easing="ease-out-sine" data-aos-offset="-350" data-aos-anchor-placement="top-center" id="judul">THREADS</h1>
+            <h1 data-aos="fade-up" data-aos-easing="ease-out-sine" data-aos-offset="-350" data-aos-anchor-placement="top-center" id="awaw">"</h1>
+            <h2 data-aos="fade-up" data-aos-easing="ease-out-sine" data-aos-offset="-300" data-aos-anchor-placement="top-center" id="tulisan">Threads become tangled, intertwined, broken off, unraveld. They constanly reflect a part of my mental state, as if they were expressing the state of human relationships.</h2>
+            <h2 data-aos="fade-right" data-aos-easing="ease-out-sine" data-aos-offset="-450" data-aos-anchor-placement="top-center" id="tahun">2019</h2>
+            <div data-aos="fade-right" data-aos-easing="ease-out-sine" data-aos-offset="-325" data-aos-anchor-placement="top-center" id="kotak1"></div>
             {/* persegi panjang kebawah */}
-            <div id="gariskotak"></div>
+            <div data-aos="fade-down" data-aos-easing="ease-out-sine" data-aos-offset="200" data-aos-anchor-placement="top-center" id="gariskotak"></div>
             {/* yang nempel di foto */}
-            <div id="lingkaran"></div>
+            <div data-aos="fade-down" data-aos-easing="ease-out-sine" data-aos-offset="-200" data-aos-anchor-placement="top-center" id="lingkaran"></div>
             {/* dibawah foto */}
-            <div id="gariskecil"></div> 
+            <div data-aos="fade-right" data-aos-easing="ease-out-sine" data-aos-offset="-400" data-aos-anchor-placement="top-center" id="gariskecil"></div> 
             {/* dibawah tulisan */}
-            <div id="hands">
+            <div data-aos="fade-left" data-aos-easing="ease-out-sine" data-aos-offset="500" data-aos-anchor-placement="top-center" id="hands">
                 <img src={IMGURL} alt="hands"/>
             </div>
         </div>
