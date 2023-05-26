7307c50eae61949a9d0b7d8f6f0aabdcde6f4266
ravinwoenarsooo <robertus.ravin@student.umn.ac.id>
2023-05-26 15:26:24 +0700

    Tio's work made a major update

--- snapo/src/components/App.jsx
+++ snapo/src/components/App.jsx
@@ -1,6 +1,14 @@
+<<<<<<< Updated upstream
 import React, { useEffect, useState } from "react";
 import Navbar from "../components/Vinik/Navbar";
+=======
+//ini komponen untuk nyusun pages main menu
+//Pages
+import React from "react";
+import Navbar from "../components/Vinik/Navbar";    
+>>>>>>> Stashed changes
 import AboutUs from "../components/NAS/AboutUs";
+
 import Tipo3 from "./Acd/Tipo3";
 import Typo1 from "./Ravin/Typo1";
 import CardTipoc from "./Calista/CardTipoc";
@@ -10,11 +18,15 @@ import Gallery from "./Ravin/Gallery";
 import Work from "./Acd/Work";
 import CardCredit from "./Calista/CardCredit";
 import Footer from "./Ravin/Footer";
+<<<<<<< Updated upstream
 import Gallery1 from "./Ravin/Gallery1"; //ini yang di link
 import Gallery2 from "./Ravin/Gallery2"; //ini yang di link
+=======
+>>>>>>> Stashed changes
 import { ScrollProvider } from "./ScrollContext";
 import LoadingScreen from './LoadingScreen';
 
+<<<<<<< Updated upstream
 function App() {
   return (
     <>
@@ -37,3 +49,28 @@ function App() {
 }
 
 export default App;
+=======
+function HomeMenu()
+{
+    return (
+        <ScrollProvider>
+            <Navbar/>{/* <Navbar/> */}
+            <Home/> {/* Corrected Finished */}
+            <Tipo3 /> {/* Corrected Finished */}
+            <Typo1/> {/* Corrected Finished */}
+            <CardTipoc /> 
+            {/* <Tipo2/> */}
+            {/* <Work/> */}
+            <CardCredit />
+            {/* <Tipoc />  */}
+            {/* <Credita /> */}
+            <Gallery/>
+            <Work/>
+            <AboutUs/> {/* Corrected Unfinished*/}
+            <Footer/>
+        </ScrollProvider>
+    )
+}
+
+export default HomeMenu;
+>>>>>>> Stashed changes
--- snapo/src/components/Ravin/Gallery.jsx
+++ snapo/src/components/Ravin/Gallery.jsx
@@ -1,28 +1,30 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Gallery(){
     const IMGURL0 = require("../../ASET/Images/borobudur.jpg");
     const IMGURL1 = require("../../ASET/Images/patung.jpg");
     const IMGURL2 = require("../../ASET/Images/lukisan3.jpg");
+    const navigate = useNavigate();
     
     function handleClickOne(){
-         alert("TES1");
+        navigate("/gallery1")
     }
     function handleClickTwo(){
-        alert("TES2");
+        navigate("/gallery2")
     }
     function handleClickThree(){
-        alert("TES3");
+        navigate("/gallery3")
     }
 
     return(
         <div id="gallery">
             <div id="group">
-                <div>
+                <button>
                     <img id="one" onClick={handleClickOne} src={IMGURL0} alt="borobudur"/>
-                </div>
+                </button>
                 <div>
-                    <img id="two" onClick={handleClickTwo} src={IMGURL1} alt="lukisan3"/>
+                    <img id="two"  onClick={handleClickTwo} src={IMGURL1} alt="lukisan3"/>
                 </div>
                 <div>
                     <img id="three" onClick={handleClickThree} src={IMGURL2} alt="patung"/>
