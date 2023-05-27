c871449307272a6d79841aa740f347da6bf988fb
acd17 <127939247+acd17@users.noreply.github.com>
2023-05-27 17:36:30 +0700

    tidK banayak perubahan

--- snapo/src/components/Ravin/Gallery.jsx
+++ snapo/src/components/Ravin/Gallery.jsx
@@ -20,15 +20,24 @@ function Gallery(){
     return(
         <div id="gallery">
             <div id="group">
-                <button>
-                    <img id="one" onClick={handleClickOne} src={IMGURL0} alt="borobudur"/>
-                </button>
-                <button>
-                    <img id="two"  onClick={handleClickTwo} src={IMGURL1} alt="lukisan3"/>
-                </button>
-                <button>
-                    <img id="three" onClick={handleClickThree} src={IMGURL2} alt="patung"/>
-                </button>
+                <div id="conta">
+                    <button>
+                            <img id="one" onClick={handleClickOne} src={IMGURL0} alt="borobudur"/>    
+                    </button>
+                    <span className="titulo" >Statue</span>
+                </div>
+                <div id="conta">
+                    <button>
+                        <img id="two"  onClick={handleClickTwo} src={IMGURL1} alt="lukisan3"/>
+                        <span className="titulo" >Art</span>
+                    </button>
+                </div>
+                <div id="conta">
+                    <button>
+                        <img id="three" onClick={handleClickThree} src={IMGURL2} alt="patung"/>
+                        <span className="titulo" >Abstrak</span>
+                    </button>
+                </div>
             </div>
         </div>
     );
