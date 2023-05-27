6e323a9cb9f5efef9049546f925aa7c438a6d116
acd17 <127939247+acd17@users.noreply.github.com>
2023-05-27 18:44:15 +0700

    sry klo error wkwkw

--- snapo/tailwind.config.js
+++ snapo/tailwind.config.js
@@ -2,12 +2,17 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
-    'node_modules/flowbite-react/*/.{js,jsx,ts,tsx}'
+    'node_modules/flowbite-react/*/.{js,jsx,ts,tsx}',
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/tw-elements/dist/js/**/*.js"
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require('flowbite/plugin')
+    require('flowbite/plugin'),
+    require("tw-elements/dist/plugin.cjs")
   ],
+  darkMode: "class"
 }
