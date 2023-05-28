713b2c7c8d9b4b7254e4f02023dab53fafeceb79
ravinwoenarsooo <robertus.ravin@student.umn.ac.id>
2023-05-28 21:57:44 +0700

    FOOTER ANIMATIONN YAYY

--- snapo/src/components/Ravin/Footer.jsx
+++ snapo/src/components/Ravin/Footer.jsx
@@ -7,41 +7,44 @@ function Footer(){
        <div id="Footer">
         <section id="footer">
             <div>
-                <img id="gm" src={gmbrf} alt="gambarfooter" />
+                <img data-aos="fade-right" data-aos-easing="ease-out-sine" data-aos-offset="-390" data-aos-anchor-placement="top-center" id="gm" src={gmbrf} alt="gambarfooter" />
             </div>
             <div id="desc">
-                <h1 id="jfoot">Reference</h1>
-                <p id="dfoot">These are the references we use in the process developing the website.</p>
+                <h1 data-aos="fade-down" data-aos-easing="ease-out-sine" data-aos-offset="-390" data-aos-anchor-placement="top-center" id="jfoot">Reference</h1>
+                <p data-aos="fade-left" data-aos-easing="ease-out-sine" data-aos-offset="-390" data-aos-anchor-placement="top-center" id="dfoot">These are the references we use in the process developing the website.</p>
             </div>
             <div id="footer1">
                 <div>
-                    <p id="Ref1">figma.com</p>
+                    <p data-aos="fade-right" data-aos-easing="ease-out-sine" data-aos-offset="-390" data-aos-anchor-placement="top-center" id="Ref1">figma.com</p>
                 </div>
                 <div>
-                    <p id="Ref2">sliderevolution.com</p>
+                    <p data-aos="fade-bottom" data-aos-easing="ease-out-sine" data-aos-offset="-390" data-aos-anchor-placement="top-center" id="Ref2">sliderevolution.com</p>
                 </div>
                 <div>
-                    <p id="Ref3">github.com</p>  
+                    <p data-aos="fade-left" data-aos-easing="ease-out-sine" data-aos-offset="-390" data-aos-anchor-placement="top-center" id="Ref3">github.com</p>  
                 </div>    
+                <div>
+                    <p data-aos="fade-left" data-aos-easing="ease-out-sine" data-aos-offset="-390" data-aos-anchor-placement="top-center" id="Ref4">Hanuman Font</p>  
+                </div> 
             </div>
             <div id="footer2">
                 <div>
-                    <p id="Ref1">AnimeJS</p>
+                    <p data-aos="fade-right" data-aos-easing="ease-out-sine" data-aos-offset="10" data-aos-anchor-placement="top-center" id="Ref1">AnimeJS</p>
                 </div>
                 <div>
-                    <p id="Ref2">TailwindCSS</p>
+                    <p data-aos="fade-right" data-aos-easing="ease-out-sine" data-aos-offset="10" data-aos-anchor-placement="top-center" id="Ref2">TailwindCSS</p>
                 </div>
                 <div>
-                    <p id="Ref3">Animation On Scroll</p>
+                    <p data-aos="fade-right" data-aos-easing="ease-out-sine" data-aos-offset="10" data-aos-anchor-placement="top-center" id="Ref3">Animation On Scroll</p>
                 </div>
                 <div>
-                    <p id="Ref4">Font Awesome Icon</p>  
+                    <p data-aos="fade-right" data-aos-easing="ease-out-sine" data-aos-offset="10" data-aos-anchor-placement="top-center" id="Ref4">Font Awesome Icon</p>  
                 </div>
                 <div>
-                    <p id="Ref5">Camille Mormal</p>  
+                    <p data-aos="fade-left" data-aos-easing="ease-out-sine" data-aos-offset="10" data-aos-anchor-placement="top-center" id="Ref5">Camille Mormal</p>  
                 </div>
                 <div>
-                    <p id="Ref6">Route</p>  
+                    <p data-aos="fade-left" data-aos-easing="ease-out-sine" data-aos-offset="10" data-aos-anchor-placement="top-center" id="Ref6">Route</p>  
                 </div>            
             </div>
             </section>
