731700b9488e98c36fda8669eeb09e30f13d406d
NAS2203 <129538027+NAS2203@users.noreply.github.com>
2023-05-28 23:06:26 +0700

    Update Home.jsx

--- snapo/src/components/Vinik/Home/Home.jsx
+++ snapo/src/components/Vinik/Home/Home.jsx
@@ -12,27 +12,25 @@ function Home () {
 
     const [cursorPosition, setCursorPosition] = useState({x:0, y:0});
     useEffect(() => {
-    anime({
-      targets: "#bg-image-about-us img",
-      translateX: -cursorPosition.x,
-      translateY: -cursorPosition.y,
-      duration: 1000,
-      easing: 'easeOutQuad',
-    });
-  }, [cursorPosition]);
+        anime({
+          targets: "#bg-home img",
+          translateX: -cursorPosition.x,
+          translateY: -cursorPosition.y,
+          duration: 1000,
+          easing: 'easeOutQuad',
+        });
+      }, [cursorPosition]);
 
     return (
-        <div id="Home">
+        <div id="Home" onMouseMove={(event)=>{
+            let movement_x = event.clientX * 0.05;
+            let movement_y = event.clientY * 0.05;
+            setCursorPosition({x: movement_x, y: movement_y})
+          }}>
             <section id="top">
-                <div onMouseMove={(event)=>{
-                    let movement_x = event.clientX * 0.05;
-                    let movement_y = event.clientY * 0.05;
-                    setCursorPosition({x: movement_x, y: movement_y})
-                }}>
                     <div id="bg-home">
                         <img className="selectDis" draggable="false" style={{position: "absolute", top: -cursorPosition.y, left: -cursorPosition.x}} src={IMGURL} alt=""/>
                     </div>
-                </div>
                 <div data-aos="fade-up" data-aos-easing="ease-out-sine" data-aos-anchor-placement="0">
                     <SvgImage id='logovertical'/>
                     <object id="background-logo"></object>
